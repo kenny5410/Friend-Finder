3931ad4f1bd92b7df5d30912fab48fd3880aa8ae
Kenny <nels5410@gmail.com>
2019-03-12 22:00:31 -0500

    Finished the application
    Finished the app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,30 @@
 var friendsArray = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
-];
+        "name":"Ryan Goodwin",
+        "photo":"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "scores":["5","1","4","4","5","1","2","5","4","1"]
+    },
+    {
+      "name":"Nayeli Reed",
+      "photo":"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "scores":["2","2","4","5","2","4","2","4","1","2"]
+    },
+    {
+      "name":"Roger Bentley",
+      "photo":"https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "scores":["4","1","4","3","2","3","5","5","4","3"]
+    },
+    {
+      "name":"Francisco Doyle",
+      "photo":"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "scores":["2","3","3","4","4","3","4","5","4","4"]
+    },
+    {
+      "name":"June Richard",
+      "photo":"https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "scores":["3","2","5","5","5","3","2","2","1","5"]
+    }
+]
 
 module.exports = friendsArray;
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -6,8 +7,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-//require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app, path);
+require("./app/routing/htmlRoutes")(app, path);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
